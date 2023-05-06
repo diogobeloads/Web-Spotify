d4cf6acaac7bf79e0359fae1e7794d62548fe195
Diogo Belo <diogobelodss22@gmail.com>
2023-05-06 13:40:38 -0300

    Adição de arquivos de midia e alteração do bg.

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
-const songName = document.getElementById("song-name");
-const bandName = document.getElementById("band-name");
-const song = document.getElementById("audio");
-const cover = document.getElementById("cover");
-const play = document.getElementById("play");
-const next = document.getElementById("next");
-const previous = document.getElementById("previous");
+const songName = document.getElementById('song-name');
+const bandName = document.getElementById('band-name');
+const song = document.getElementById('audio');
+const cover = document.getElementById('disc-cover');
+const play = document.getElementById('play');
+const next = document.getElementById('next');
+const previous = document.getElementById('previous');
+const currentProgress = document.getElementById('current-progress');
+const progressContainer = document.getElementById('progress-container');
+const shuffleButton = document.getElementById('shuffle');
+const likeButton = document.getElementById('like');
 
 // Variáveis auxiliares
 const herbocinetica = {
@@ -26,9 +30,62 @@ const puteiro = {
     songName: "Puteiro em Joao Pessoa",
     file: "puteiro",
     artist: "Raimundos",
-  };
+};
+const mais_pedida = {
+  songName: "A Mais Pedida",
+  file: "mais_pedida",
+  artist: "Raimundos",
+};
+const deixa_eu_falar = {
+  songName: "Deixa eu Falar",
+  file: "deixa_eu_falar",
+  artist: "Raimundos",
+};
+const kombao = {
+  songName: "Pitando no Kombao",
+  file: "kombao",
+  artist: "Raimundos",
+};
+const palhas = {
+  songName: "Palhas do Coqueiro",
+  file: "palhas",
+  artist: "Raimundos",
+};
+const baile = {
+  songName: "Baile Funky",
+  file: "baile",
+  artist: "Raimundos",
+};
+const tora = {
+  songName: "Tora Tora",
+  file: "tora",
+  artist: "Raimundos",
+};
+const esporrei = {
+  songName: "Esporrei na Manivela",
+  file: "esporrei",
+  artist: "Raimundos",
+};
+const meLambe = {
+  songName: "Me Lambe",
+  file: "meLambe",
+  artist: "Raimundos",
+};
+const bestinha = {
+  songName: "Bestinha",
+  file: "bestinha",
+  artist: "Raimundos",
+};
+const fases = {
+  songName: "Mulher de Fases",
+  file: "fases",
+  artist: "Raimundos",
+};
+
 let isPlaying = false;
-const playlist = [herbocinetica,poquitoMas,rapante,puteiro];
+let isShuffled = false;
+const originalplaylist = [herbocinetica,poquitoMas,rapante,puteiro,mais_pedida,deixa_eu_falar,kombao,palhas,baile,tora,esporrei,meLambe,bestinha,fases];
+let sortedPlaylist = [... originalplaylist];
 let currentSong = 0;
 
 function playSong() {
@@ -54,14 +111,14 @@ function playPauseDecider() {
 }
 
 function initializeSong() {
-    songName.innerText = playlist[currentSong].songName;
-    bandName.innerText = playlist[currentSong].artist;
-    song.src = `/songs/${playlist[currentSong].file}.mp3`;
-    cover.src = `/images/${playlist[currentSong].file}.webp`;
+    songName.innerText = sortedPlaylist[currentSong].songName;
+    bandName.innerText = sortedPlaylist[currentSong].artist;
+    song.src = `/songs/${sortedPlaylist[currentSong].file}.mp3`;
+    cover.src = `/images/${sortedPlaylist[currentSong].file}.jpg`;
 }
 
 function nextSong() {
-    if(currentSong === playlist.length -1){
+    if(currentSong === sortedPlaylist.length -1){
         currentSong = 0;
     }
     else {
@@ -73,7 +130,7 @@ function nextSong() {
 
 function previousSong() {
     if(currentSong === 0){
-        currentSong = playlist.length - 1;
+        currentSong = sortedPlaylist.length - 1;
     }
     else {
         currentSong -= 1;
@@ -81,9 +138,47 @@ function previousSong() {
     initializeSong();
     playSong();
 }
+function updateProgressBar(){
+  const barWidth = (song.currentTime/song.duration)*100;
+  currentProgress.style.setProperty('--progress', `${barWidth}%`);
+}
+function jumpTo(event){
+  const width = progressContainer.clientWidth;
+  const clickPosition = event.offsetX;
+  const jumpToTime = (clickPosition/width)* song.duration;
+  song.currentTime = jumpToTime;
+}
+function shuffleArray(preShuffleArray){
+  const size = preShuffleArray.length;
+  let currentIndex = size - 1;
+  while(currentIndex > 0){
+   let randomIndex = Math.floor(Math.random()*size);
+   let aux = preShuffleArray[currentIndex];
+   preShuffleArray [currentIndex] = preShuffleArray[randomIndex];
+   preShuffleArray[randomIndex] = aux;
+   currentIndex -= 1;
+  }
+}
+
+
+function shuffleButtonClicked(){
+  if(isShuffled === false){
+    isShuffled = true;
+    shuffleArray(sortedPlaylist);
+    shuffleButton.classList.add('button-active');
+  }
+  else {
+    isShuffled = false;
+    sortedPlaylist= [...originalplaylist];
+    shuffleButton.classList.remove('button-active');
+  }
+}
 
 initializeSong();
 
 play.addEventListener("click", playPauseDecider);
 next.addEventListener("click", nextSong);
 previous.addEventListener("click", previousSong);
+song.addEventListener('timeupdate',updateProgressBar);
+progressContainer.addEventListener('click',jumpTo);
+shuffleButton.addEventListener('click', shuffleButtonClicked);
